2543e959cf3ffc3244a9a0e79d5f9eb184719cef
Barry Deen <barrydeen@protonmail.com>
2024-12-21 09:59:05 -0500

    add relay to listen and serve

--- main.go
+++ main.go
@@ -184,7 +184,7 @@ PREMIUM RELAY & BLOSSOM SERVER
 	})
 
 	log.Println("🚀 Server started on port", relayPort)
-	http.ListenAndServe("0.0.0.0:"+relayPort, mux)
+	http.ListenAndServe("0.0.0.0:"+relayPort, relay)
 
 }
 
